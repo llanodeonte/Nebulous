72d246f033f7ed0fde6a11675657e9466005f003
llanodeonte <bishuvey@gmail.com>
2022-11-20 08:40:13 -0600

    Added modules and basic ram testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod cpu;
+mod bus;
+mod ram;
+
+use bus::Bus;
+use ram::Ram;
+
 fn main() {
-    println!("Hello, world!");
+    let bus = Bus::new();
+    let mut ram = Ram::new();
+
+    // Ram testing
+    let test_byte = bus.read_ram(&ram, 0xFF);
+    println!("Test byte: {:X?}", test_byte);
+
+    bus.write_ram(&mut ram, 0xFF, 0xA1);
+
+    let test_byte = bus.read_ram(&ram, 0xFF);
+    println!("Test byte: {:X?}", test_byte);
 }
