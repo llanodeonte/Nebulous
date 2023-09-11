26f6720b3aa4074e2fb3ffebb27cf39d9f0b3c29
llanodeonte <bishuvey@gmail.com>
2023-09-11 15:20:31 -0600

    Built in basic ROM loading

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod cpu;
 mod bus;
 mod ram;
 
+use std::env;
+use std::fs;
+use std::io::Read;
+
 use cpu::Cpu;
 use bus::Bus;
 use ram::Ram;
@@ -10,6 +14,12 @@ fn main() {
     let cpu = Cpu::new();
     let bus = Bus::new();
     let mut ram = Ram::new();
+
+    // Load and store ROM data
+    let rom_file_path = env::args().nth(1).unwrap();
+    let mut rom_file = fs::File::open(rom_file_path).unwrap();
+    let mut rom_data = Vec::new();
+    rom_file.read_to_end(&mut rom_data).unwrap();
 
     // Ram test prior to writing to Ram
     let test_byte = bus.read_ram(&ram, 0xFF);
@@ -25,4 +35,7 @@ fn main() {
     // CPU testing
     let test_pc = cpu.tick(&bus, &ram);
     println!("Test pc: {:X?}", test_pc);
+
+    // ROM load test
+    println!("ROM file name: {:X?}", rom_data);
 }
