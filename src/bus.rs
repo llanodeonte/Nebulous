09ce0afa5ccdc80a52401757f265621bf0e40796
llanodeonte <bishuvey@gmail.com>
2023-09-14 13:11:52 -0600

    Began working on memory map for bus

--- src/bus.rs
+++ src/bus.rs
@@ -1,5 +1,15 @@
 // Add memory map guards based on addressing
 
+// $0000–$07FF 	$0800 	2 KB internal RAM
+// $0800–$0FFF 	$0800 	
+// $1000–$17FF 	$0800   Mirrors of $0000–$07FF
+// $1800–$1FFF 	$0800
+// $2000–$2007 	$0008 	NES PPU registers
+// $2008–$3FFF 	$1FF8 	Mirrors of $2000–$2007 (repeats every 8 bytes)
+// $4000–$4017 	$0018 	NES APU and I/O registers
+// $4018–$401F 	$0008 	APU and I/O functionality that is normally disabled. See CPU Test Mode.
+// $4020–$FFFF 	$BFE0 	Cartridge space: PRG ROM, PRG RAM, and mapper registers
+
 use crate::ram::Ram;
 
 pub struct Bus {
@@ -11,8 +21,13 @@ impl Bus {
         }
     }
 
+    // Update naming conventions to reflect broader addressing
     pub fn read_ram(&self, ram: &Ram, addr: usize) -> u8 {
-        ram.read_ram(addr)
+        match addr {
+            // Finish building full address range with temp panics for each function
+            0x0000..=0x1FFF => ram.read_ram(addr),
+            _ => panic!("Address {:?} outside valid range", addr)
+        }
     }
 
     pub fn write_ram(&self, ram: &mut Ram, addr: usize, data: u8) {
