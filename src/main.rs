07a41cc00ef7036ad2e199344424f8750d1bab4d
llanodeonte <bishuvey@gmail.com>
2023-09-22 11:56:32 -0600

    Extended test data for LDA ABS opcode

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,18 @@ fn main() {
     bus.write(&mut ram, 0x0000, 0xA9); // LDA IMM
     bus.write(&mut ram, 0x0001, 0x2B); // Random data
     bus.write(&mut ram, 0x0002, 0xA5); // LDA ZPG
-    bus.write(&mut ram, 0x0003, 0xC4); // ZPG Addr
+    bus.write(&mut ram, 0x0003, 0xC4); // ZPG addr
     bus.write(&mut ram, 0x00C4, 0x57); // Random data
     bus.write(&mut ram, 0x0004, 0xB5); // LDA ZPX
-    bus.write(&mut ram, 0x0005, 0x1F); // ZPX Base Addr
-    cpu.x = 0x10;                                 // ZPX Offset
+    bus.write(&mut ram, 0x0005, 0x1F); // ZPX base addr
+    cpu.x = 0x10;                                 // ZPX offset
     bus.write(&mut ram, 0x002F, 0x34); // Random data
-
-    let mut loop_limit = 3;
+    bus.write(&mut ram, 0x0006, 0xAD); // LDA ABS
+    bus.write(&mut ram, 0x0007, 0x47); // ABS addr lo
+    bus.write(&mut ram, 0x0008, 0x01); // ABS addr hi
+    bus.write(&mut ram, 0x0147, 0x17); // Random data
+    
+    let mut loop_limit = 4;
 
     cpu.debug_print();
 
